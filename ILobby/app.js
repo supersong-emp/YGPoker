a8638523c1762feb56a8ffc1dbc5daafd65fd723
supersong-emp <ptarowana@proton.me>
2023-04-21 17:44:14 +0900

    Update app.js

--- ILobby/app.js
+++ ILobby/app.js
@@ -69,8 +69,8 @@ app.use('/announcement', require('./routes/announcement'));
 //global.strGameAddress = 'http://157.230.38.106:5555';
 global.strGameAddress = 'https://ygpo888.net';
 
-global.strBig2Address = 'http://localhost:5556';
-//global.strBig2Address = 'https://ygpokerbig2.net';
+//global.strBig2Address = 'http://localhost:5556';
+global.strBig2Address = 'https://ygpokerbig2.net';
 
 let Instance = require('./game/ILobby');
 const { get } = require('http');
